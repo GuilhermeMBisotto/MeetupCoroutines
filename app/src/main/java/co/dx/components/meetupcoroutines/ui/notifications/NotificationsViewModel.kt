2d45cc410be72ca4863c7992762af438db0253c7
guilhermebisotto <guibisotto@gmail.com>
2019-08-10 14:50:03 -0300

    * Initial commit * Created Project

---
+++ app/src/main/java/co/dx/components/meetupcoroutines/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package co.dx.components.meetupcoroutines.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
